be891b70686f2f88ae6e56375b4c9b3bd79d4fe3
athul rajesh <athul.rjsh@gmail.com>
2020-11-11 22:51:23 +0530

    todo completed

---
+++ src/Form.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Form = ({ inputText, todos, setTodos, setInputText, setStatus }) => {
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      {
+        text: inputText,
+        completed: false,
+        id: Math.random() * 1000,
+      },
+    ]);
+    setInputText("");
+  };
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  };
+  return (
+    <form>
+      <input
+        value={inputText}
+        onChange={inputTextHandler}
+        type="text"
+        className="todo-input"
+      />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select
+          onBlur={statusHandler}
+          onChange={statusHandler}
+          className="filter-todo"
+          name="todos"
+        >
+          <option value="All">All</option>
+          <option value="completed">completed</option>
+          <option value="uncompleted">uncompleted</option>
+        </select>
+      </div>
+    </form>
+  );
+};
+export default Form;
